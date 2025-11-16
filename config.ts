5fbde624d28d1b053d199f5d11686c28320ccae5
theofficialvedantjoshi <vedant.messi101@gmail.com>
2025-11-16 18:33:34 +0530

    fix: default environment bug

--- config.ts
+++ config.ts
@@ -1,22 +1,10 @@
 /* eslint-disable n/no-process-env */
+import dotenv from "dotenv";
+import moduleAlias from "module-alias";
 
-import path from 'path';
-import dotenv from 'dotenv';
-import moduleAlias from 'module-alias';
-
-
-// Check the env
-const NODE_ENV = (process.env.NODE_ENV ?? 'development');
-
-// Configure "dotenv"
-const result2 = dotenv.config({
-  path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
-});
-if (result2.error) {
-  throw result2.error;
-}
+dotenv.config();
 
 // Configure moduleAlias
-if (__filename.endsWith('js')) {
-  moduleAlias.addAlias('@src', __dirname + '/dist');
+if (__filename.endsWith("js")) {
+  moduleAlias.addAlias("@src", __dirname + "/dist");
 }
